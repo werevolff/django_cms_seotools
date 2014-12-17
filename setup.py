372f48edff26f5c9f5a914fe055d72e5cec5dc26
Nikolay Dolganov <nd@dvhb.ru>
2014-12-17 14:29:56 +1000

    Som fixes in setup.py and requirements.txt files

--- setup.py
+++ setup.py
@@ -14,12 +14,13 @@ install_reqs = parse_requirements(os.path.join(CURRENT_DIR, 'requirements.txt'))
 reqs = [str(ir.req) for ir in install_reqs]
 
 setup(
-    name='Django CMS SEO Tools',
-    version='1.0',
+    name='djangocms_seotools',
+    version='1.4',
     description='SEO and Custom tools for the Django CMS',
     author='Nikolay <Werevolff> Dolganov',
     author_email='sirnikolasd@yandex.ru',
     url='https://github.com/werevolff/django_cms_seotools',
     packages=['djangocms_seotools', 'djangocms_seotools.tests'],
+    data_files=[('Requirements', ['requirements.txt'])],
     install_requires=reqs
 )
